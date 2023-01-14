15c83597113e66b28294abb063bc3bc9d9707351
SamRzeszutek <borzeszutek@gmail.com>
2023-01-14 13:34:28 -0500

    Added gripper subsystem for opening and closing

---
+++ src/main/java/frc/robot/subsystems/Gripper.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+// Open and close gripper 
+
+public class Gripper extends SubsystemBase {
+  /** Creates a new Gripper. */
+  DoubleSolenoid gripper; 
+  public Gripper() {
+    gripper=new DoubleSolenoid(null, 0,1); 
+
+    
+  }
+
+  // Declaring my gripper as an double solonoid
+
+
+
+
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void openNoseCone() {
+    gripper.set(DoubleSolenoid.Value.kReverse);
+  }
+
+  public void closeNoseCone() {
+    gripper.set(DoubleSolenoid.Value.kForward);
+  }
+
+}
