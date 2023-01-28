bfd0721abb5e9ce46408da24ce777c5cf5abcdee
MotorMouthMerc <antonioliberatore04@gmail.com>
2023-01-28 12:55:17 -0500

    change

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,28 +5,30 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
-public static final double GRIPPER_ENCODER_MAX = 4; 
 
-public static final double GRIPPER_ENCODER_MIN = 0;
+  public static final double GRIPPER_ENCODER_MAX = 4;
 
-public static final double EXTENDER_SPEED = 1; 
+  public static final double GRIPPER_ENCODER_MIN = 0;
+
+  public static final double EXTENDER_SPEED = 1;
   public static final double EXTENDER_LOW_POSITION = 5; // dummy VARIABLE
-public static final double EXTENDER_MED_POSITION = 10; // DUMMY VARIABLE 
-public static final double EXTENDER_HIGH_POSITION = 15; // DUMMY VARIABLE
-  
+  public static final double EXTENDER_MED_POSITION = 10; // DUMMY VARIABLE
+  public static final double EXTENDER_HIGH_POSITION = 15; // DUMMY VARIABLE
+  public static final double EXTENDER_START_POSITION = 0; // DUMMY VARIABLE
 
 }
-
-
-
