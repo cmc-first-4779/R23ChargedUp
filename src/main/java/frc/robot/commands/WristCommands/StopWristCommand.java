33d0b799ae318e003a3b00f82afac2081d0b3a76
MotorMouthMerc <antonioliberatore04@gmail.com>
2023-02-04 11:00:06 -0500

    Made changes

--- src/main/java/frc/robot/commands/WristCommands/StopWristCommand.java
+++ src/main/java/frc/robot/commands/WristCommands/StopWristCommand.java
@@ -2,42 +2,27 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.WristCommands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-import frc.robot.subsystems.ExtenderSubsystem;
-
-public class extendByJoyStickCommand extends CommandBase {
-  ExtenderSubsystem m_extenderSubsystem;
-
-  /** Creates a new lowExtenderCommand. */
-  public extendByJoyStickCommand(ExtenderSubsystem extenderSubsystem) {
-    m_extenderSubsystem = extenderSubsystem;
+public class StopWristCommand extends CommandBase {
+  /** Creates a new StopWristCommand. */
+  public StopWristCommand() {
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(m_extenderSubsystem);
-
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-
-  }
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-
-    m_extenderSubsystem.extendArm();
-
-  }
+  public void execute() {}
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-    m_extenderSubsystem.stopMotor();
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
