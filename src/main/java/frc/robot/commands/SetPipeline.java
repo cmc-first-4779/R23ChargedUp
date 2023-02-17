e16688b7e2fe05d0a060f210e444da75e10c0245
MotorMouthMerc <antonioliberatore04@gmail.com>
2023-02-17 18:54:30 -0500

    Create Pipeline Command

--- src/main/java/frc/robot/commands/SetPipeline.java
+++ src/main/java/frc/robot/commands/SetPipeline.java
@@ -4,21 +4,37 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.LimelightSubsystem;
 
 public class SetPipeline extends CommandBase {
+  boolean isDone = false;
+  LimelightSubsystem myLimelight;
+  int pipeline;
+
+
   /** Creates a new SetPipeline. */
-  public SetPipeline() {
+  public SetPipeline(LimelightSubsystem limelight, int pipeline) {
+    myLimelight = limelight;
+    int myPipeline = pipeline;
+
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(limelight);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    myLimelight.setPipeline(pipeline);
+
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    SmartDashboard.putNumber("GetPipeline", pipeline);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
@@ -27,6 +43,6 @@ public class SetPipeline extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return isDone;
   }
 }
