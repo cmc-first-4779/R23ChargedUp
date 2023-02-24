f23d79f7774a8aa3ca9f7ce409015543aaf59f7d
mbhoxsie <32553272+mbhoxsie@users.noreply.github.com>
2023-02-23 20:03:12 -0500

    commented out example command and ss

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -84,8 +84,8 @@ public class RobotContainer {
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+  //  new Trigger(m_exampleSubsystem::exampleCondition)
+    //    .onTrue(new ExampleCommand(m_exampleSubsystem));
     m_driverController.L1().whileTrue(new ShoulderLower(shoulderSubsystem));
     m_driverController.R1().whileTrue(new ShoulderRaise(shoulderSubsystem));
     m_driverController.cross().onTrue(new ShoulderStopCommand(shoulderSubsystem));
@@ -96,7 +96,7 @@ public class RobotContainer {
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is
     // pressed,
     // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    //m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
 
   /**
