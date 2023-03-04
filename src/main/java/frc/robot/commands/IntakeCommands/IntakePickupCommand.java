c992b749c744d4af18dd69d3d7159f5192ca91eb
mbhoxsie <32553272+mbhoxsie@users.noreply.github.com>
2023-03-04 12:49:10 -0500

    cleaned up Intake commands

--- src/main/java/frc/robot/commands/IntakeCommands/IntakePickupCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommands/IntakePickupCommand.java
@@ -36,7 +36,7 @@ public class IntakePickupCommand extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    intakeSubsystem.stopMotor();
+    //intakeSubsystem.stopMotor();
   }
 
   // Returns true when the command should end.
