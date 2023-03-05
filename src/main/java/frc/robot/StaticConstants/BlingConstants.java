b65b2bf755a01935009c5f85f0c0d48c828d251b
mbhoxsie <32553272+mbhoxsie@users.noreply.github.com>
2023-03-05 07:53:52 -0500

    Moved StaticConstants folder

--- src/main/java/frc/robot/StaticConstants/BlingConstants.java
+++ src/main/java/frc/robot/StaticConstants/BlingConstants.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.StaticConstants;
+package frc.robot.StaticConstants;
 
 /**
  * Table for all of the available colors and palettes on the Blinkin' LED Driver.
