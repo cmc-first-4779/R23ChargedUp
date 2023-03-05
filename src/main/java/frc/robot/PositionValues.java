8d707ba5b3bcdd4abccfad0023622a10a3209c88
mbhoxsie <32553272+mbhoxsie@users.noreply.github.com>
2023-03-05 08:22:01 -0500

    added comments

--- src/main/java/frc/robot/PositionValues.java
+++ src/main/java/frc/robot/PositionValues.java
@@ -4,7 +4,10 @@
 
 package frc.robot;
 
-// Encoder Positions for Shoulder,Extender,Wrist
+// THIS CLASS CONTAINES OUR SETPOINTS FOR ALL OF OUR POSITIONS FOR THE 
+//    SHOULDER, EXTENDER, AND WRIST.
+//
+// THE VALUES ARE SORTED BY POSITION TO MAKE THEM EASIER TO FIND AND TUNE
 
 public class PositionValues {
 
@@ -37,33 +40,33 @@ public class PositionValues {
     public static final double SHOULDER_POSITION_LOW_CONE_NODE = 42500;
     public static final double EXTENDER_POSITION_LOW_CONE_NODE = 2.60;
     public static final double WRIST_POSITION_LOW_CONE_NODE = 56.07;
-    
+
     // MID_CONE_Position for each subsystem
     public static final double SHOULDER_POSITION_MID_CONE_NODE = 77500;
     public static final double EXTENDER_POSITION_MID_CONE_NODE = 1.83;
     public static final double WRIST_POSITION_MID_CONE_NODE = 53.07;
 
-    //   HIGH_CONE_NODE_Position for each subsystem
+    // HIGH_CONE_NODE_Position for each subsystem
     public static final double SHOULDER_POSITION_HIGH_CONE_NODE = 92500;
     public static final double EXTENDER_POSITION_HIGH_CONE_NODE = 49.05;
     public static final double WRIST_POSITION_HIGH_CONE_NODE = 48.07;
 
-     //   LOW_CUBE_NODE_Position for each subsystem
+    // LOW_CUBE_NODE_Position for each subsystem
     public static final double SHOULDER_POSITION_LOW_CUBE_NODE = 42500;
     public static final double EXTENDER_POSITION_LOW_CUBE_NODE = 9.07;
     public static final double WRIST_POSITION_LOW_CUBE_NODE = 13.95;
 
-     //   MID_CUBE_NODE_Position for each subsystem
+    // MID_CUBE_NODE_Position for each subsystem
     public static final double SHOULDER_POSITION_MID_CUBE_NODE = 57500;
     public static final double EXTENDER_POSITION_MID_CUBE_NODE = 23.00;
     public static final double WRIST_POSITION_MID_CUBE_NODE = 27.07;
 
-     //   HIGH_CUBE_NODE Position for each subsystem
+    // HIGH_CUBE_NODE Position for each subsystem
     public static final double SHOULDER_POSITION_HIGH_CUBE_NODE = 80000;
     public static final double EXTENDER_POSITION_HIGH_CUBE_NODE = 75.43;
     public static final double WRIST_POSITION_HIGH_CUBE_NODE = 35.00;
 
-     //   Position for each subsystem
+    // HUMAN_PLAYER_STATION Position for each subsystem
     public static final double SHOULDER_POSITION_HUMAN_PLAYER_STATION = 32500;
     public static final double EXTENDER_POSITION_HUMAN_PLAYER_STATION = 0.0;
     public static final double WRIST_POSITION_HUMAN_PLAYER_STATION = 8.5;
