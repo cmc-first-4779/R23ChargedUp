fee384b4def289e63ed17b66634c495501927463
mbhoxsie <32553272+mbhoxsie@users.noreply.github.com>
2023-03-23 18:48:14 -0400

    Create a new autointake

--- src/main/java/frc/robot/commands/IntakeCommands/AutoIntakeSetSpeed.java
+++ src/main/java/frc/robot/commands/IntakeCommands/AutoIntakeSetSpeed.java
@@ -4,55 +4,92 @@
 
 package frc.robot.commands.IntakeCommands;
 
-import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
-import frc.robot.commands.MiscCommands.Wait;
 import frc.robot.subsystems.IntakeSubsystem;
 
-//  This Command Group embeds a Timer into the IntakeSetSpeed Command so that we can use 
-//    the command in Auton so that it will end.
-//   (NOTE:  THIS IS A PARALLEL RACE GROUP)
-public class AutoIntakeSetSpeed extends ParallelRaceGroup {
+public class AutoIntakeSetSpeed extends CommandBase {
   IntakeSubsystem intake;
   String intakeMode;
-  double timer;
-  /** Creates a new AutoIntakeSetSpeed. */
+  // Declare our timer
+  Timer timer = new Timer();
+  // Declare local variable for keeping track of the time (seconds)
+  private double intakeRunTime;
+  // Declare our Intake Set Speed
+  double intakeSpeed;
+
+  /** Creates a new AutoIntakeSetSpeed2. */
   public AutoIntakeSetSpeed(IntakeSubsystem intake, String intakeMode) {
-    //  Set our local variables to the pass-thru
+    // Set our local variables to the pass-thru
     this.intake = intake;
     this.intakeMode = intakeMode;
-    
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(intake);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
     // Set up a switch statement to set our timer value to the appropriate
     // constant based on the intakeMode that is passed in.
     // (We are doing this to simplify our commands)
     switch (intakeMode) {
       // Set the value for the Eject Cone
       case "EJECT_CONE":
-        timer = Constants.EJECT_CONE_AUTON_TIMER;
+        intakeRunTime = Constants.EJECT_CONE_AUTON_TIMER;
+        intakeSpeed = Constants.EJECT_CONE_SPEED;
         break;
       // Set the value for the Eject Cube
       case "EJECT_CUBE":
-        timer = Constants.EJECT_CUBE_AUTON_TIMER;
+        intakeRunTime = Constants.EJECT_CUBE_AUTON_TIMER;
+        intakeSpeed = Constants.EJECT_CUBE_SPEED;
         break;
       // Set the value for the Intake Cone
       case "INTAKE_CONE":
-        timer = Constants.INTAKE_CONE_AUTON_TIMER;
+        intakeRunTime = Constants.INTAKE_CONE_AUTON_TIMER;
+        intakeSpeed = Constants.INTAKE_CONE_SPEED;
         break;
       // Stop the Intake
       case "INTAKE_STOP":
-        timer = 0.1;  //  Make this one really short
+        intakeRunTime = 0.1; // Make this one really short
+        intakeSpeed = 0;
         break;
       // Default mode is Intake a Cube
       default:
-        timer = Constants.INTAKE_CUBE_AUTON_TIMER;
+        intakeRunTime = Constants.INTAKE_CUBE_AUTON_TIMER;
+        intakeSpeed = Constants.INTAKE_CUBE_SPEED;
         break;
     }
+    // Run our Intake
+    intake.intakeRun(intakeSpeed);
+    // Timer reset and start
+    timer.reset(); // Reset the timer.
+    timer.start(); // Start the timer.
+  }
 
-    // Add your commands in the addCommands() call, e.g.
-    // addCommands(new FooCommand(), new BarCommand());
-    addCommands(
-      new IntakeSetSpeed(intake, intakeMode),
-      new Wait(timer)
-    );
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    timer.stop(); // Stop the timer
+    intake.stopMotor();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    // If our timer is less than the time we want it to run, keep running the
+    // command.
+    if (timer.get() < intakeRunTime) {
+      return false;
+    }
+    // Else once we hit our alloted time, exit out of the command.
+    else
+      return true;
   }
 }
